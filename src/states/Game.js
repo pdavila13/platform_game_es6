e7e3c02a790db4ca3adff05e624edbaad5bc9f6c
Paolo Dávila <pdavila@iesebre.com>
2017-05-05 16:18:38 +0200

    Added world ground

--- src/states/Game.js
+++ src/states/Game.js
@@ -6,14 +6,19 @@ export default class extends Phaser.State {
   init () {}
   preload () {
     this.game.load.spritesheet('player', './assets/images/player.png', 28, 22)
+    this.game.load.image('ground', './assets/images/ground.png')
   }
 
   create () {
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
     this.player = this.game.add.sprite(250, 50, 'player')
 
+    this.game.add.sprite(760/2-160,400/2,'ground')
+
     game.physics.arcade.enable(this.player)
     this.player.body.gravity.y = 600
+
+    this.player.body.setSize(20, 20, 0, 0);
 
     this.player.animations.add('idle', [3, 4, 5, 4], 5, true)
     this.player.animations.play('idle')
