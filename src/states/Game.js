77b3f4765d472c986fe145c70c9f6ef3c6e42fb9
Paolo Dávila <pdavila@iesebre.com>
2017-05-09 20:58:25 +0200

    Solved problem particles OK

--- src/states/Game.js
+++ src/states/Game.js
@@ -14,6 +14,9 @@ export default class extends Phaser.State {
     this.game.load.image('enemy', './assets/images/enemy.png')
     this.game.load.image('coin', './assets/images/coin.png')
 
+    this.game.load.image('dust', './assets/images/dust.png')
+    this.game.load.image('exp', './assets/images/exp.png')
+
     this.game.load.audio('jump', ['./assets/audio/jump.wav', './assets/audio/jump.mp3'])
     this.game.load.audio('dust', ['./assets/audio/dust.wav', './assets/audio/dust.mp3'])
     this.game.load.audio('coin', ['./assets/audio/coin.wav', './assets/audio/coin.mp3'])
@@ -25,9 +28,7 @@ export default class extends Phaser.State {
     this.player = this.game.add.sprite(360, 100, 'player')
     this.enemy = this.game.add.sprite(490, 400/2-20, 'enemy')
 
-    this.jumpSound = this.game.add.audio('jump', 0.1)
-    this.dustSound = this.game.add.audio('dust', 0.1)
-    this.coinSound = this.game.add.audio('coin', 0.1)
+    this.addSounds()
 
     game.physics.arcade.enable(this.player)
     game.physics.arcade.enable(this.enemy)
@@ -56,6 +57,9 @@ export default class extends Phaser.State {
     if (this.player.body.touching.down && this.player.y > 100) {
       if (this.hasJumped) {
         this.dustSound.play()
+        this.dust.x = this.player.x
+        this.dust.y = this.player.y+10
+        this.dust.start(true, 300, null, 8)
       }
       this.hasJumped = false
     }
@@ -117,18 +121,25 @@ export default class extends Phaser.State {
     }
   }
 
-  setParticles () {
-    this.dust = this.game.add.emitter(0, 0, 20);
-    this.dust.makeParticles('dust');
-    this.dust.setYSpeed(-100, 100);
-    this.dust.setXSpeed(-100, 100);
-    this.dust.gravity = 0;
+  addSounds () {
+    this.jumpSound = this.game.add.audio('jump', 0.1)
+    this.dustSound = this.game.add.audio('dust', 0.1)
+    this.expSound = this.game.add.audio('exp', 0.1)
+    this.coinSound = this.game.add.audio('coin', 0.1)
+  }
 
-    this.exp = this.game.add.emitter(0, 0, 20);
-    this.exp.makeParticles('exp');
-    this.exp.setYSpeed(-150, 150);
-    this.exp.setXSpeed(-150, 150);
-    this.exp.gravity = 0;
+  setParticles () {
+    this.dust = this.game.add.emitter(0, 0, 20)
+    this.dust.makeParticles('dust')
+    this.dust.setYSpeed(-100, 100)
+    this.dust.setXSpeed(-100, 100)
+    this.dust.gravity = 0
+
+    this.exp = this.game.add.emitter(0, 0, 20)
+    this.exp.makeParticles('exp')
+    this.exp.setYSpeed(-150, 150)
+    this.exp.setXSpeed(-150, 150)
+    this.exp.gravity = 0
   }
 
   render () {
